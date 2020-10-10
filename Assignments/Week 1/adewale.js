3c3d2b2a4c6975557b84a090ccc55a0e441ef81e
classyrazy <66884694+classyrazy@users.noreply.github.com>
2020-10-10 15:58:50 -0500

    Update adewale.js

--- Assignments/Week 1/adewale.js
+++ Assignments/Week 1/adewale.js
@@ -1,2 +1,2 @@
 var farenInput = prompt("farenheit(°F)");
-alert("The temperature is " + Math.floor((farenInput - 32) * 5/9) + "°C in celcius");
+alert("The temperature is " + ((farenInput - 32) * 5/9) + "°C in celcius");
